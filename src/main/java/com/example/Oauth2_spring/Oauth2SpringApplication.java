ad316ca5baf3bb061d8a9c49ab1c0de0e1803134
Kotala Santhosh <143922100+santhoshkotala123@users.noreply.github.com>
2024-11-07 18:00:53 +0530

    frist data

---
+++ src/main/java/com/example/Oauth2_spring/Oauth2SpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.Oauth2_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2SpringApplication.class, args);
+	}
+
+}
